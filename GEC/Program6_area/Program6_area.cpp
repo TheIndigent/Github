8c6c98e144467daf8f4439adabc8cba8df9648e3
TheIndigent <91890466+TheIndigent@users.noreply.github.com>
2021-10-26 14:11:02 +0100

    Add files via upload

---
+++ GEC/Program6_area/Program6_area.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int width = 15;
+    int height = 25;
+    int area = width * height;
+    cout << "If you times: " << width << "(the width) by: " << height << "(the height) you will get: " << area << endl;
+}
---
+++ GEC/Program8_pi/Program8_pi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const float pi = 3.14159f;
+    float radius = 25.5f;
+    float area = pi * radius * radius;
+    cout << "The area of a curcle with a radius of " << radius << " is " << area << endl;
+}
