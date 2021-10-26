af86b792712d2a2300387aa608fd35e101414da8
TheIndigent <91890466+TheIndigent@users.noreply.github.com>
2021-10-26 14:39:06 +0100

    Add files via upload

---
+++ GEC/week3 - shareofsavings/week3 - shareofsavings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float mySavings = 2000;
+    float yourPercentage = 50;
+    float yourShare = (mySavings * (yourPercentage / 100));
+
+    cout << "your share: " << yourShare << endl;
+}
